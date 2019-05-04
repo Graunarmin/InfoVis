6830b46388f42367670e5bfea8f58e5a5fb29f55
Johanna Sacher <johanna.sacher@uni-weimar.de>
2019-05-04 20:35:38 +0200

    alles ein bisschen durch die Gegend geschoben und Zeug ausprobiert, zu müde um weiter zu machen, committe nur zur Sicherheit. Mache morgen weiter

--- InfoVis_Assignment_SS15/src/infovis/scatterplot/View.java
+++ InfoVis_Assignment_SS15/src/infovis/scatterplot/View.java
@@ -5,16 +5,18 @@ import infovis.debug.Debug;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.Color;
 
 import javax.swing.JPanel;
 
 public class View extends JPanel {
 	     private Model model = null;
 	     private Rectangle2D markerRectangle = new Rectangle2D.Double(0,0,0,0);
-	     private Rectangle2D matrixFrame = new Rectangle2D.Double(135,25,650,650);
-	     private Line2D horizontalSegmentLine = new Line2D.Double(135,25,785,25);
-	     private Line2D verticalSegmentLine = new Line2D.Double(135,32,135,681);
+	     private Rectangle2D matrixFrame = new Rectangle2D.Double(100,25,650,650);
+	     private Line2D horizontalSegmentLine = new Line2D.Double(100,25,750,25);
+	     private Line2D verticalSegmentLine = new Line2D.Double(100,32,100,681);
 
 		 public Rectangle2D getMarkerRectangle() {
 			return markerRectangle;
@@ -28,6 +30,7 @@ public class View extends JPanel {
 
 
 			Graphics2D g2D = (Graphics2D) g;
+			g2D.clearRect(0, 0, getWidth(), getHeight());  //Delete everything before update
 
 			//Matrix Frame
 			g2D.draw(matrixFrame);
@@ -47,7 +50,7 @@ public class View extends JPanel {
 
 			//draw the vertical lines into the matrix
 			for (int i = 0; i < dim; i++){	//Cheating a bit by taking out the last line so it doesn't look ugly
-				g2D.translate(segmentLineDistance, 0);	// <------ bekomme es einfach nicht hin das die Abschnitte gleich verteilt sind >.<
+				g2D.translate(segmentLineDistance, 0);
 
 				//taking out the last line while keeping the right distances
 				if(i != dim-1){
@@ -64,7 +67,7 @@ public class View extends JPanel {
 
 			//Horizontal labels
 			for (String l : model.getLabels()) {
-				g2D.drawString(l,220+i,25);	//Draws labels starting at position and height
+				g2D.drawString(l,180+i,25);	//Draws labels starting at position and height
 				i += (650/(dim-2));	//moves next label a bit to the right
 			}
 
@@ -74,18 +77,32 @@ public class View extends JPanel {
 
 			//Vertical labels
 			for (String l : model.getLabels()) {
-				g2D.drawString(l, 1150, 100+j);
-				j += (650/(dim-2));
+				g2D.drawString(l, 1150, 100 + j);
+				j += (650 / (dim - 2));
 			}
-			
 
+
+//			for (Data d : model.getList()) {
+//				for (Point2D points : carSpecs.get(d.getLabel().hashCode()) ) {
+//					g.setColor(Color.BLUE);
+//					g.drawOval((int)points.getX(), (int)points.getY(), 2, 2);
+//					g.fillOval((int)points.getX(), (int)points.getY(), 2, 2);
+//				}
+//			}
+
+			Debug.print("Ranges [smallest and greatest value]: ");
+			Debug.println("");
 			for (Range range : model.getRanges()) {
-				Debug.print(range.toString() + "+");
+
+				Debug.print(range.toString());
 				Debug.print(",  ");
 				Debug.println("");
 			}
+
+			Debug.print("Lists: ");
+			Debug.println("");
 			for (Data d : model.getList()) {
-				Debug.print(d.toString() + "#");
+				Debug.print(d.toString());
 				Debug.println("");
 			}
 	        
